b865039fe8c9c15dd5b1acc8b556714c74f6fffd
muhsin-life <123951517+muhsin-life@users.noreply.github.com>
2023-12-19 23:29:14 +0530

    Add files via upload

---
+++ src/components/hooks/useData.tsx
@@ -0,0 +1,46 @@
+import getProducts from "@/helpers/api/getProductsData";
+import { BrandsProps } from "@/types/brands";
+import { CategoryProps } from "@/types/categories";
+import { ProductProps } from "@/types/products";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export const useProducts = (type_key: string, slug: string) => {
+  const { locale } = useRouter();
+
+  return useQuery({
+    queryKey: ["get-products", slug],
+    queryFn: async () =>
+      (await getProducts(type_key, slug, locale as locale)) as ProductProps,
+  });
+};
+
+export const useCategories = () => {
+  const { locale } = useRouter();
+  return useQuery({
+    queryKey: ["get-categories"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://prodapp.lifepharmacy.com/api/web/categories?lang=${locale}`
+      );
+
+      return data as CategoryProps;
+    },
+  });
+};
+
+export const useBrands = () => {
+  const { locale } = useRouter();
+  return useQuery({
+    queryKey: ["get-brands"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://prodapp.lifepharmacy.com/api/web/brands?lang=${locale}`
+      );
+
+      return data as BrandsProps;
+    },
+    enabled:false
+  });
+};
